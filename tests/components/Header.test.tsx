807b51ce6fe54c916ecc263e36f3b7dafd2c63f1
marcel <chukwuma.marcel@quickcheck.ng>
2024-06-23 10:42:24 +0100

    improving-assertion-in-header-test

--- tests/components/Header.test.tsx
+++ tests/components/Header.test.tsx
@@ -5,10 +5,12 @@ import Header from "../../src/components/Header";
 
 describe("Header", () => {
     it("should render the welcome message when the Header component is mounted.", () => {
-      mockAuthStore({ firstName: 'Marcel', lastName: 'Chukwuma', email: 'marcel.chukwuma00@gmail.com' });
+        const user = { firstName: 'Marcel', lastName: 'Chukwuma', email: 'marcel.chukwuma00@gmail.com' }
+        mockAuthStore(user);
 
-      render(<Header />);
+        render(<Header />);
   
-      expect(screen.getByText(/welcome,/i)).toBeInTheDocument();
+        expect(screen.getByText(/welcome,/i)).toBeInTheDocument();
+        expect(screen.getByText(user.firstName)).toBeInTheDocument();
     })
 });
